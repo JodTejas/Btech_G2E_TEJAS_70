1ec3234888a81fa46c22262446bcdab2009a9d50
JodTejas <tejassingh0444@gmail.com>
2026-02-10 16:01:47 +0530

    Add files via upload
    Code 8 of lab activity list.

---
+++ Even_or_Odd.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("ENTER NUMBER :- ");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("Number is divisible by 2. ");
+    }
+    else {
+        printf("Number is not divisible by 2.");
+    }
+    
+    return 0;
+}
