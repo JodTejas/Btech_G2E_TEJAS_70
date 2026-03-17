06fc79ca89f0f1fb5f7dcbeadadc76eeee0fa1a6
JodTejas <tejassingh0444@gmail.com>
2026-03-17 15:58:06 +0530

    Add files via upload
    CLASS WORK QUES

---
+++ SumOfNaturalNO..c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n,sum=0;  
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("Sum of numbers from 0 to %d is: %d\n", n, sum);
+    
+    return 0;
+}
